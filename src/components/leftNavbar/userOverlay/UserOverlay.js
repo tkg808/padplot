e20419f6d825b00a77bf9108fcc4146942b531c4
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-07-20 17:24:03 -0700

    Add items and style to UserOverlay

--- src/components/leftNavbar/userOverlay/UserOverlay.js
+++ src/components/leftNavbar/userOverlay/UserOverlay.js
@@ -5,7 +5,19 @@ export default function UserOverlay()
 
   return (
     <div className="user-overlay-container">
-      "Hello from UserOverlay"
+      <div className="user-overlay-section user-overlay-top-section">
+        <div className="user-overlay-item">My projects</div>
+        <div className="user-overlay-item">Become a member</div>
+      </div>
+      <div className="user-overlay-section user-overlay-middle-section">
+        <div className="user-overlay-item">Find contractors</div>
+        <div className="user-overlay-item">Market news</div>
+        <div className="user-overlay-item">Stats</div>
+        <div className="user-overlay-item">Settings</div>
+      </div>
+      <div className="user-overlay-section user-overlay-bottom-section">
+        <div className="user-overlay-item">Sign up</div>
+      </div>
     </div>
   );
 }
